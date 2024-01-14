e9e3f07b016cc9f189da2e084b18719265524833
tarekmohsni <tarekmohsni100@gmail.com>
2024-01-14 18:08:38 +0100

    config projet

---
+++ horloge_back/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(cors());
+
+app.listen(PORT,() => {
+
+    console.log(`Server is Successfully Running, and App is listening on port ${PORT}`);
+
+});
